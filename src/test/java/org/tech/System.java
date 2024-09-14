3cb995acc03ba3befe1d7fd84f14313618bb2f31
banupriyatamilarasan <banupriyasr90@gmail.com>
2024-09-14 17:40:01 +0530

    Command

---
+++ src/test/java/org/tech/System.java
@@ -0,0 +1,16 @@
+package org.tech;
+
+public class System {
+	private void empName() {
+		System.out.println("vel");
+	}
+	private void empName1() {
+		System.out.println("mahi");
+	}
+	private void empName2() {
+		System.out.println("ram");
+		
+	}
+	}
+
+
